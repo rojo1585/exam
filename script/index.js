abb29426796155a027cfab02c2a93ae7a729f7c3
rojo1585 <jorge_mendoza1585@hotmail.com>
2021-10-01 10:52:57 -0500

    img estatica, ajuste de resoluciones

--- script/index.js
+++ script/index.js
@@ -2,13 +2,4 @@ $('.carousel').carousel({
     interval: 2000
   })
 
-  const nav = document.querySelector('nav');
-
-function watchMenu() {
-  nav.style.top = `-${
-    nav.clientHeight < window.scrollY ?
-      nav.clientHeight : 0
-  }px`;
-}
-
-window.addEventListener('scroll', watchMenu);
+  
